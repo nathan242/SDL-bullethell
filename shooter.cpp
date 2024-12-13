c2054c2b192164c752bcfcc479d9eb3e750760f6
Nathan Patrizi <nathan.patrizi@gmail.com>
2024-12-13 13:33:34 +0000

    Change enemy set up logic

--- shooter.cpp
+++ shooter.cpp
@@ -30,150 +30,155 @@ int SHOT_PHYS_DELAY = 2500000;
 #define NUM_SHOTS 40
 #define NUM_SHOTS_ENEMY 40
 
-#define ENEMY_SET_COUNT 5
-#define ENEMY_SET_SIZE 5
+#define ENEMY_SET_COUNT 4
+
+#define MAX_ENEMY_SLOTS 10
 
 bool game_over = false;
 
-void activate_enemy_set(enemy *enemy_set[ENEMY_SET_SIZE], int set_id)
+void activate_enemy_set(engine_obj_list *enemy_slots[MAX_ENEMY_SLOTS], int set_id, engine *eng, projectile_manager *enemy_shot_mngr, powerup_double_shot *powerup_double_shot_obj, powerup_quad_spread_shot *powerup_quad_spread_shot_obj)
 {
     switch (set_id)
     {
         case 0:
-            enemy_set[0]->pos_x = 150;
-            enemy_set[0]->pos_y = enemy_set[0]->size_y*-1;
-            enemy_set[0]->step_x = 1;
-            enemy_set[0]->step_y = 1;
-            enemy_set[0]->draw_active = true;
-            enemy_set[0]->phys_active = true;
-            enemy_set[0]->move_x_last = {0, 0};
-            enemy_set[0]->move_y_last = {0, 0};
-            enemy_set[1]->pos_x = 300;
-            enemy_set[1]->pos_y = enemy_set[1]->size_y*-2;
-            enemy_set[1]->step_x = -1;
-            enemy_set[1]->step_y = 1;
-            enemy_set[1]->draw_active = true;
-            enemy_set[1]->phys_active = true;
-            enemy_set[1]->move_x_last = {0, 0};
-            enemy_set[1]->move_y_last = {0, 0};
-            enemy_set[2]->pos_x = 650;
-            enemy_set[2]->pos_y = enemy_set[2]->size_y*-3;
-            enemy_set[2]->step_x = 0;
-            enemy_set[2]->step_y = 1;
-            enemy_set[2]->draw_active = true;
-            enemy_set[2]->phys_active = true;
-            enemy_set[2]->move_x_last = {0, 0};
-            enemy_set[2]->move_y_last = {0, 0};
+            delete enemy_slots[0]->obj;
+            delete enemy_slots[1]->obj;
+            delete enemy_slots[2]->obj;
+
+            enemy_slots[0]->obj = new enemy(eng, enemy_shot_mngr);
+            enemy_slots[0]->obj->init();
+
+            enemy_slots[1]->obj = new enemy_diagonal(eng, enemy_shot_mngr);
+            enemy_slots[1]->obj->init();
+
+            enemy_slots[2]->obj = new enemy_cargo(eng, enemy_shot_mngr);
+            enemy_slots[2]->obj->init();
+            ((enemy*)(enemy_slots[2]->obj))->drop_powerup = powerup_double_shot_obj;
+
+            enemy_slots[0]->obj->pos_x = 150;
+            enemy_slots[0]->obj->pos_y = enemy_slots[0]->obj->size_y*-1;
+            enemy_slots[0]->obj->step_x = 1;
+            enemy_slots[0]->obj->step_y = 1;
+            enemy_slots[0]->obj->draw_active = true;
+            enemy_slots[0]->obj->phys_active = true;
+            enemy_slots[0]->obj->move_x_last = {0, 0};
+            enemy_slots[0]->obj->move_y_last = {0, 0};
+            enemy_slots[1]->obj->pos_x = 300;
+            enemy_slots[1]->obj->pos_y = enemy_slots[1]->obj->size_y*-2;
+            enemy_slots[1]->obj->step_x = -1;
+            enemy_slots[1]->obj->step_y = 1;
+            enemy_slots[1]->obj->draw_active = true;
+            enemy_slots[1]->obj->phys_active = true;
+            enemy_slots[1]->obj->move_x_last = {0, 0};
+            enemy_slots[1]->obj->move_y_last = {0, 0};
+            enemy_slots[2]->obj->pos_x = 650;
+            enemy_slots[2]->obj->pos_y = enemy_slots[2]->obj->size_y*-3;
+            enemy_slots[2]->obj->step_x = 0;
+            enemy_slots[2]->obj->step_y = 1;
+            enemy_slots[2]->obj->draw_active = true;
+            enemy_slots[2]->obj->phys_active = true;
+            enemy_slots[2]->obj->move_x_last = {0, 0};
+            enemy_slots[2]->obj->move_y_last = {0, 0};
 
             break;
 
         case 1:
-            enemy_set[0]->pos_x = 150;
-            enemy_set[0]->pos_y = enemy_set[0]->size_y*-1;
-            enemy_set[0]->step_x = 1;
-            enemy_set[0]->step_y = 1;
-            enemy_set[0]->draw_active = true;
-            enemy_set[0]->phys_active = true;
-            enemy_set[0]->move_x_last = {0, 0};
-            enemy_set[0]->move_y_last = {0, 0};
-            enemy_set[1]->pos_x = 300;
-            enemy_set[1]->pos_y = enemy_set[1]->size_y*-3;
-            enemy_set[1]->step_x = -1;
-            enemy_set[1]->step_y = 1;
-            enemy_set[1]->draw_active = true;
-            enemy_set[1]->phys_active = true;
-            enemy_set[1]->move_x_last = {0, 0};
-            enemy_set[1]->move_y_last = {0, 0};
-            enemy_set[2]->pos_x = 100;
-            enemy_set[2]->pos_y = enemy_set[2]->size_y*-5;
-            enemy_set[2]->step_x = -1;
-            enemy_set[2]->step_y = 1;
-            enemy_set[2]->draw_active = true;
-            enemy_set[2]->phys_active = true;
-            enemy_set[2]->move_x_last = {0, 0};
-            enemy_set[2]->move_y_last = {0, 0};
+            delete enemy_slots[0]->obj;
+            delete enemy_slots[1]->obj;
+            delete enemy_slots[2]->obj;
+
+            enemy_slots[0]->obj = new enemy_diagonal(eng, enemy_shot_mngr);
+            enemy_slots[0]->obj->init();
+
+            enemy_slots[1]->obj = new enemy_diagonal(eng, enemy_shot_mngr);
+            enemy_slots[1]->obj->init();
+
+            enemy_slots[2]->obj = new enemy_diagonal(eng, enemy_shot_mngr);
+            enemy_slots[2]->obj->init();
+
+            enemy_slots[0]->obj->pos_x = 150;
+            enemy_slots[0]->obj->pos_y = enemy_slots[0]->obj->size_y*-1;
+            enemy_slots[0]->obj->step_x = 1;
+            enemy_slots[0]->obj->step_y = 1;
+            enemy_slots[0]->obj->draw_active = true;
+            enemy_slots[0]->obj->phys_active = true;
+            enemy_slots[0]->obj->move_x_last = {0, 0};
+            enemy_slots[0]->obj->move_y_last = {0, 0};
+            enemy_slots[1]->obj->pos_x = 300;
+            enemy_slots[1]->obj->pos_y = enemy_slots[1]->obj->size_y*-3;
+            enemy_slots[1]->obj->step_x = -1;
+            enemy_slots[1]->obj->step_y = 1;
+            enemy_slots[1]->obj->draw_active = true;
+            enemy_slots[1]->obj->phys_active = true;
+            enemy_slots[1]->obj->move_x_last = {0, 0};
+            enemy_slots[1]->obj->move_y_last = {0, 0};
+            enemy_slots[2]->obj->pos_x = 100;
+            enemy_slots[2]->obj->pos_y = enemy_slots[2]->obj->size_y*-5;
+            enemy_slots[2]->obj->step_x = -1;
+            enemy_slots[2]->obj->step_y = 1;
+            enemy_slots[2]->obj->draw_active = true;
+            enemy_slots[2]->obj->phys_active = true;
+            enemy_slots[2]->obj->move_x_last = {0, 0};
+            enemy_slots[2]->obj->move_y_last = {0, 0};
 
             break;
 
         case 2:
-            enemy_set[0]->pos_x = 150;
-            enemy_set[0]->pos_y = enemy_set[0]->size_y*-1;
-            enemy_set[0]->step_x = 0;
-            enemy_set[0]->step_y = 1;
-            enemy_set[0]->draw_active = true;
-            enemy_set[0]->phys_active = true;
-            enemy_set[0]->move_x_last = {0, 0};
-            enemy_set[0]->move_y_last = {0, 0};
-            enemy_set[1]->pos_x = 550;
-            enemy_set[1]->pos_y = enemy_set[0]->size_y*-4;
-            enemy_set[1]->step_x = 0;
-            enemy_set[1]->step_y = 1;
-            enemy_set[1]->draw_active = true;
-            enemy_set[1]->phys_active = true;
-            enemy_set[1]->move_x_last = {0, 0};
-            enemy_set[1]->move_y_last = {0, 0};
+            delete enemy_slots[0]->obj;
+            delete enemy_slots[1]->obj;
+
+            enemy_slots[0]->obj = new enemy_diagonal_stationary(eng, enemy_shot_mngr);
+            enemy_slots[0]->obj->init();
+
+            enemy_slots[1]->obj = new enemy_diagonal_stationary(eng, enemy_shot_mngr);
+            enemy_slots[1]->obj->init();
+            ((enemy*)(enemy_slots[1]->obj))->drop_powerup = powerup_quad_spread_shot_obj;
+
+            enemy_slots[0]->obj->pos_x = 150;
+            enemy_slots[0]->obj->pos_y = enemy_slots[0]->obj->size_y*-1;
+            enemy_slots[0]->obj->step_x = 0;
+            enemy_slots[0]->obj->step_y = 1;
+            enemy_slots[0]->obj->draw_active = true;
+            enemy_slots[0]->obj->phys_active = true;
+            enemy_slots[0]->obj->move_x_last = {0, 0};
+            enemy_slots[0]->obj->move_y_last = {0, 0};
+            enemy_slots[1]->obj->pos_x = 550;
+            enemy_slots[1]->obj->pos_y = enemy_slots[0]->obj->size_y*-4;
+            enemy_slots[1]->obj->step_x = 0;
+            enemy_slots[1]->obj->step_y = 1;
+            enemy_slots[1]->obj->draw_active = true;
+            enemy_slots[1]->obj->phys_active = true;
+            enemy_slots[1]->obj->move_x_last = {0, 0};
+            enemy_slots[1]->obj->move_y_last = {0, 0};
 
             break;
 
         case 3:
-            enemy_set[0]->pos_x = 150;
-            enemy_set[0]->pos_y = enemy_set[0]->size_y*-1;
-            enemy_set[0]->step_x = 0;
-            enemy_set[0]->step_y = 1;
-            enemy_set[0]->draw_active = true;
-            enemy_set[0]->phys_active = true;
-            enemy_set[0]->move_x_last = {0, 0};
-            enemy_set[0]->move_y_last = {0, 0};
-            enemy_set[1]->pos_x = 550;
-            enemy_set[1]->pos_y = enemy_set[0]->size_y*-4;
-            enemy_set[1]->step_x = 0;
-            enemy_set[1]->step_y = 1;
-            enemy_set[1]->draw_active = true;
-            enemy_set[1]->phys_active = true;
-            enemy_set[1]->move_x_last = {0, 0};
-            enemy_set[1]->move_y_last = {0, 0};
+            enemy_slots[0]->obj = new enemy_diagonal_stationary_fwdsprd(eng, enemy_shot_mngr);
+            enemy_slots[0]->obj->init();
+
+            enemy_slots[1]->obj = new enemy_diagonal_stationary_fwdsprd(eng, enemy_shot_mngr);
+            enemy_slots[1]->obj->init();
+
+            enemy_slots[0]->obj->pos_x = 150;
+            enemy_slots[0]->obj->pos_y = enemy_slots[0]->obj->size_y*-1;
+            enemy_slots[0]->obj->step_x = 0;
+            enemy_slots[0]->obj->step_y = 1;
+            enemy_slots[0]->obj->draw_active = true;
+            enemy_slots[0]->obj->phys_active = true;
+            enemy_slots[0]->obj->move_x_last = {0, 0};
+            enemy_slots[0]->obj->move_y_last = {0, 0};
+            enemy_slots[1]->obj->pos_x = 550;
+            enemy_slots[1]->obj->pos_y = enemy_slots[0]->obj->size_y*-4;
+            enemy_slots[1]->obj->step_x = 0;
+            enemy_slots[1]->obj->step_y = 1;
+            enemy_slots[1]->obj->draw_active = true;
+            enemy_slots[1]->obj->phys_active = true;
+            enemy_slots[1]->obj->move_x_last = {0, 0};
+            enemy_slots[1]->obj->move_y_last = {0, 0};
 
             break;
     }
-}
-
-void init_all_enemy_sets(enemy *enemy_sets[ENEMY_SET_COUNT][ENEMY_SET_SIZE], engine *eng, projectile_manager *enemy_shot_mngr, powerup_double_shot *powerup_double_shot_obj, powerup_quad_spread_shot *powerup_quad_spread_shot_obj)
-{
-    enemy_sets[0][0] = new enemy(eng, enemy_shot_mngr);
-    enemy_sets[0][0]->init();
-    eng->add_object(enemy_sets[0][0]);
-    enemy_sets[0][1] = new enemy_diagonal(eng, enemy_shot_mngr);
-    enemy_sets[0][1]->init();
-    eng->add_object(enemy_sets[0][1]);
-    enemy_sets[0][2] = new enemy_cargo(eng, enemy_shot_mngr);
-    enemy_sets[0][2]->init();
-    enemy_sets[0][2]->drop_powerup = powerup_double_shot_obj;
-    eng->add_object(enemy_sets[0][2]);
-
-    enemy_sets[1][0] = new enemy_diagonal(eng, enemy_shot_mngr);
-    enemy_sets[1][0]->init();
-    eng->add_object(enemy_sets[1][0]);
-    enemy_sets[1][1] = new enemy_diagonal(eng, enemy_shot_mngr);
-    enemy_sets[1][1]->init();
-    eng->add_object(enemy_sets[1][1]);
-    enemy_sets[1][2] = new enemy_diagonal(eng, enemy_shot_mngr);
-    enemy_sets[1][2]->init();
-    eng->add_object(enemy_sets[1][2]);
-
-    enemy_sets[2][0] = new enemy_diagonal_stationary(eng, enemy_shot_mngr);
-    enemy_sets[2][0]->init();
-    eng->add_object(enemy_sets[2][0]);
-    enemy_sets[2][1] = new enemy_diagonal_stationary(eng, enemy_shot_mngr);
-    enemy_sets[2][1]->init();
-    enemy_sets[2][1]->drop_powerup = powerup_quad_spread_shot_obj;
-    eng->add_object(enemy_sets[2][1]);
-
-    enemy_sets[3][0] = new enemy_diagonal_stationary_fwdsprd(eng, enemy_shot_mngr);
-    enemy_sets[3][0]->init();
-    eng->add_object(enemy_sets[3][0]);
-    enemy_sets[3][1] = new enemy_diagonal_stationary_fwdsprd(eng, enemy_shot_mngr);
-    enemy_sets[3][1]->init();
-    eng->add_object(enemy_sets[3][1]);
 }
 
 void shooter()
@@ -208,7 +213,10 @@ void shooter()
     powerup_double_shot *powerup_double_shot_obj;
     powerup_quad_spread_shot *powerup_quad_spread_shot_obj;
 
-    enemy *enemy_sets[ENEMY_SET_COUNT][ENEMY_SET_SIZE] = {};
+    engine_obj_list *enemy_slots[MAX_ENEMY_SLOTS];
+    for (int i = 0; i < MAX_ENEMY_SLOTS; i++) {
+        enemy_slots[i] = eng->add_object(new engine_obj());
+    }
 
     powerup_double_shot_obj = new powerup_double_shot(eng);
     powerup_double_shot_obj->init();
@@ -221,8 +229,7 @@ void shooter()
     eng->add_object(ship_obj);
     ship_obj->init();
 
-    init_all_enemy_sets(enemy_sets, eng, enemy_shot_mngr, powerup_double_shot_obj, powerup_quad_spread_shot_obj);
-    activate_enemy_set(enemy_sets[active_enemy_set], active_enemy_set);
+    activate_enemy_set(enemy_slots, active_enemy_set, eng, enemy_shot_mngr, powerup_double_shot_obj, powerup_quad_spread_shot_obj);
 
     for (int shot_count = 0; shot_count < NUM_SHOTS; shot_count++) {
         shots[shot_count] = new player_projectile();
@@ -310,12 +317,10 @@ void shooter()
             }
 
             init_enemy_set = true;
-            for (int i = 0; i < ENEMY_SET_SIZE; i++) {
-                if (enemy_sets[active_enemy_set][i] == NULL) continue;
 
-                if (enemy_sets[active_enemy_set][i]->draw_active) {
+            for (int i = 0; i < MAX_ENEMY_SLOTS; i++) {
+                if (enemy_slots[i]->obj->draw_active) {
                     init_enemy_set = false;
-                    break;
                 }
             }
 
@@ -324,7 +329,7 @@ void shooter()
             }
 
             if (init_enemy_set) {
-                activate_enemy_set(enemy_sets[active_enemy_set], active_enemy_set);
+                activate_enemy_set(enemy_slots, active_enemy_set, eng, enemy_shot_mngr, powerup_double_shot_obj, powerup_quad_spread_shot_obj);
             }
 
             // Redraw screen
@@ -334,7 +339,6 @@ void shooter()
 
     SDL_Quit();
 
-    delete eng;
     delete ship_obj;
     delete player_shot_mngr;
     delete enemy_shot_mngr;
@@ -349,13 +353,11 @@ void shooter()
         delete enemy_shots[enemy_shot_count];
     }
 
-    for (int s = 0; s < ENEMY_SET_COUNT; s++) {
-        for (int i = 0; i < ENEMY_SET_SIZE; i++) {
-            if (enemy_sets[s][i] == NULL) continue;
-
-            delete enemy_sets[s][i];
-        }
+    for (int i = 0; i < MAX_ENEMY_SLOTS; i++) {
+        delete enemy_slots[i]->obj;
     }
+
+    delete eng;
 
     return;
 }
