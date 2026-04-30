5674d8f9f4fd332c03a24d1e3d91d9ef23002698
Nathan Patrizi <nathan.patrizi@gmail.com>
2026-04-30 20:24:20 +0100

    Make paused text screen size independent

--- game_over.cpp
+++ game_over.cpp
@@ -11,7 +11,7 @@ void game_over::init()
 
     size_x = 115;
     size_y = 20;
-    pos_y = (i_eng->area_y / 2) -10;
+    pos_y = (i_eng->area_y / 2) - 10;
     pos_x = (i_eng->area_x / 2) - 57;
     texture = (SDL_Texture*)i_eng->get_resource("game_over_tex");
 }
